82dfcf7a4cd57281ccaf59185d5dc4bd46c948b4
Sam Bonnekamp <sam@bonnekamp.net>
2025-11-25 23:09:26 +0800

    switched from fork to clone3
    oh my god this was so fucking hard to do. You can see the remnants of
    me trying to fracture the stack space with mmap but that always caused
    the cloned process to segfault

--- main.c
+++ main.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <linux/sched.h>    /* Definition of struct clone_args */
+#include <sched.h>          /* Definition of CLONE_* constants */
+#include <sys/syscall.h>    /* Definition of SYS_* constants */
+#include <sys/mman.h>
+#include <stdint.h>
+#include <string.h>
+
+#define STACK_SIZE 8192
 
 int child_stuff(){
-  char *argv[] = {"/bin/sh", 0};
-  if(execve("/bin/sh", argv, NULL) < 0)
+  char *argv[] = {"/bin/bash", 0};
+  if(execve("/bin/bash", argv, NULL) < 0)
     perror("execve");
-  return 0;
+  _exit(0);
 }
 
 int main(int argc, char* argv[]){
@@ -16,8 +24,24 @@ int main(int argc, char* argv[]){
     return 1;
   }
   printf("executing \"%s\"\n", argv[1]);
-  //fork
-  pid_t pid = fork();
+
+  //childs stack
+  char *stack = mmap(NULL,
+                     STACK_SIZE,
+                     PROT_READ | PROT_WRITE,
+                     MAP_PRIVATE | MAP_ANON,
+                     -1,
+                     0);
+  if(stack == MAP_FAILED){
+    perror("[FATAL] mmap");
+    return 1;
+  }
+  char* top_of_stack = stack + STACK_SIZE;
+  struct clone_args cl_args = {
+    .flags = CLONE_NEWUTS,
+    .exit_signal = SIGCHLD
+  };
+  long pid = syscall(SYS_clone3, &cl_args, sizeof(cl_args));
   switch(pid){
   case -1:
     perror("[FATAL] Couldn't create child");
@@ -29,15 +53,16 @@ int main(int argc, char* argv[]){
     break;
   default:
     //parent
-    int wstatus;
-    printf("waiting on: %d\n", pid);
-    pid_t ret_val = wait(&wstatus);
-
+    ;
+    siginfo_t wstatus = {0};
+    printf("waiting on: %ld\n", pid);
+    pid_t ret_val = waitid(P_PID, pid, &wstatus, WEXITED);
+    if(ret_val < 0){
+      perror("[FATAL] waitid");
+      break;
+    }
     //exit handling
-    printf("%d exited ", ret_val);
-    if(WIFEXITED(wstatus))
-      printf("with status %d", WEXITSTATUS(wstatus));
-    fputs("\n", stdout);
+    printf("%ld exited with status %d: %s\n", pid, wstatus.si_status, strsignal(wstatus.si_status));
     break;
   }
 }
